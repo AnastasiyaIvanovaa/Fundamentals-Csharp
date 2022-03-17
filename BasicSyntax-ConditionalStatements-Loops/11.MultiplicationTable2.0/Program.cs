24733bad0682a16c07d2bd7087fa23a8161dba73
Anastasiya.Ivanova <agb66666@gmail.com>
2022-03-17 16:51:27 +0200

    Added the solutions for exercises 5 to 13.

---
+++ BasicSyntax-ConditionalStatements-Loops/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11.MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            if (multiplier <= 10)
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    Console.WriteLine($"{integer} X {i} = {integer * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{integer} X {multiplier} = {integer * multiplier}");
+            }
+        }
+    }
+}
+
---
+++ BasicSyntax-ConditionalStatements-Loops/9.SumOfOddNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 2 * n;
+            int sum = 0;
+            for (int i = 1; i <= count; i += 2)
+            {
+                sum += i;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
