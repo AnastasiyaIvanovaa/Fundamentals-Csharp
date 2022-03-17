da0511456eeb76a6426e6c5d3b33af30c145d58e
Anastasiya.Ivanova <agb66666@gmail.com>
2022-03-17 13:17:16 +0200

    Added the solution of 4.Back in 30 minutes.

---
+++ BasicSyntax-ConditionalStatements-Loops/4.BackIn30Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4.BackIn30Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int currentMinutes = minutes + 30;
+            if (currentMinutes>=60)
+            {
+                hours += 1;
+                minutes = currentMinutes- 60;
+                if (hours==24)
+                {
+                    hours = 0;
+                }
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
