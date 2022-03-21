b189d1db5627fd8631e500819580651d25e96d45
Anastasiya.Ivanova <agb66666@gmail.com>
2022-03-21 12:48:28 +0200

    Added 2 solutions.

---
+++ Lists-MoreExercises/01.Messaging/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListsFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            string text = Console.ReadLine();
+            string result = "";
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int sum = 0;
+                int current = numbers[i];
+                while (current > 0)
+                {
+                    int digit = current % 10;
+                    sum += digit;
+                    current /= 10;
+                }
+                if (sum >= text.Length)
+                {
+                    sum -= text.Length;
+                }
+                result += text.Substring(sum, 1);
+                text = text.Remove(sum, 1);
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Lists-MoreExercises/2.CarRace/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListsFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            double firstTime = 0;
+            double secondTime = 0;
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    firstTime *= 0.8;
+                }
+                else
+                {
+                    firstTime += numbers[i];
+                }
+            }
+            for (int i = numbers.Count - 1; i > numbers.Count / 2; i--)
+            {
+                if (numbers[i] == 0)
+                {
+                    secondTime *= 0.8;
+                }
+                else
+                {
+                    secondTime += numbers[i];
+                }
+            }
+            if (firstTime < secondTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {Math.Round(firstTime, 1)}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {Math.Round(secondTime, 1)}");
+
+            }
+        }
+    }
+}
