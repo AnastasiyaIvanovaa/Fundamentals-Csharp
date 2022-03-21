44b9ad8c165ef3846aec6e44f0db4f0546d04643
Anastasiya.Ivanova <agb66666@gmail.com>
2022-03-21 12:10:28 +0200

    Added the last solution of the lab.

---
+++ Lists-Lab/07.ListManipulationAdvanced/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.ListManipulationAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbersOriginal = numbers;
+            string input = Console.ReadLine();
+            while (input!="end")
+            {
+                string[] command = input.Split();
+                if (command[0]=="Contains")
+                {
+                    if (numbers.Contains(int.Parse(command[1])))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No such number");
+                    }
+                }
+                else if (command[0]=="PrintEven")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 == 0)));
+                }
+                else if (command[0]=="PrintOdd")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)));
+                }
+                else if (command[0]=="GetSum")
+                {
+                    int sum = 0;
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        sum += numbers[i];
+                    }
+                    Console.WriteLine(sum);
+                }
+                else if (command[0]=="Filter")
+                {
+                    if (command[1]=="<")
+                    {
+                        Console.WriteLine(string.Join(" ",numbers.Where(x=>x<int.Parse(command[2]))));
+                    }
+                    else if (command[1]==">")
+                    {
+                        Console.WriteLine(string.Join(" ", numbers.Where(x => x > int.Parse(command[2]))));
+
+                    }
+                    else if (command[1]==">=")
+                    {
+                        Console.WriteLine(string.Join(" ", numbers.Where(x => x >= int.Parse(command[2]))));
+                    }
+                    else if (command[1]=="<=")
+                    {
+                        Console.WriteLine(string.Join(" ", numbers.Where(x => x <= int.Parse(command[2]))));
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            if (numbers!=numbersOriginal)
+            {
+                Console.WriteLine(string.Join(" ",numbers));
+            }
+        }
+    }
+}
